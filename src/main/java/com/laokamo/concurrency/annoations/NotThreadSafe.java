3e8b052432bd353d91383970c99e4c456c216537
laokamo <578144621@qq.com>
2019-02-15 17:02:17 +0800

    新增注解

---
+++ src/main/java/com/laokamo/concurrency/annoations/NotThreadSafe.java
@@ -0,0 +1,16 @@
+package com.laokamo.concurrency.annoations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 线程不安全标记注解
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
+public @interface NotThreadSafe {
+
+    String value() default "";
+}
---
+++ src/main/java/com/laokamo/concurrency/annoations/Recommend.java
@@ -0,0 +1,16 @@
+package com.laokamo.concurrency.annoations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 推荐的写法注解
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
+public @interface Recommend {
+
+    String value() default "";
+}
