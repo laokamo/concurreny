281ab051349a894f550010c92b8d9508364fc675
laokamo <578144621@qq.com>
2019-02-17 10:19:03 +0800

    不可变对象

---
+++ src/main/java/com/laokamo/concurrency/immutable/ImmutableExample1.java
@@ -0,0 +1,35 @@
+package com.laokamo.concurrency.immutable;
+
+import com.google.common.collect.Maps;
+import com.laokamo.concurrency.annoations.NotThreadSafe;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Map;
+
+@Slf4j
+@NotThreadSafe
+public class ImmutableExample1 {
+
+    private static final Integer a = 1;
+    private static final String b = "2";
+    private static final Map<Integer, Integer> map = Maps.newHashMap();
+
+    static {
+        map.put(1, 2);
+        map.put(2, 4);
+        map.put(3, 5);
+    }
+
+    public static void main(String[] args) {
+        //a = 2;
+        //b = "555";
+        //map = Maps.newHashMap();
+        map.put(1, 6);
+        log.info("{}", map.get(1));
+    }
+
+
+    public void test(final int d) {
+        //d = 2;
+    }
+}
---
+++ src/main/java/com/laokamo/concurrency/immutable/ImmutableExample3.java
@@ -0,0 +1,22 @@
+package com.laokamo.concurrency.immutable;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
+import com.laokamo.concurrency.annoations.ThreadSafe;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@ThreadSafe
+public class ImmutableExample3 {
+
+    private static final ImmutableList<Integer> list = ImmutableList.of(1, 2, 3);
+
+    private static final ImmutableSet set = ImmutableSet.copyOf(list);
+
+    public static void main(String[] args) {
+        list.add(4);
+        set.add(6);
+    }
+
+
+}
