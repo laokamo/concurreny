b4b0a74b6d398f77d01ccfe762f7be7b871370ee
laokamo <578144621@qq.com>
2019-02-18 14:29:07 +0800

    并发容器

---
+++ src/main/java/com/laokamo/concurrency/example/concurrent/ConcurrentHashMapExample.java
@@ -0,0 +1,47 @@
+package com.laokamo.concurrency.example.concurrent;
+
+import com.laokamo.concurrency.annoations.ThreadSafe;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Map;
+import java.util.concurrent.*;
+
+@Slf4j
+@ThreadSafe
+public class ConcurrentHashMapExample {
+
+    // 请求总数
+    private static int clientTotal = 5000;
+    // 同时并发执行的线程数
+    private static int threadTotal = 200;
+
+    private static Map map = new ConcurrentHashMap();
+
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        final Semaphore semaphore = new Semaphore(threadTotal);
+        final CountDownLatch countDownLatch = new CountDownLatch(clientTotal);
+        for (int i = 0; i < clientTotal; i++) {
+            int finalI = i;
+            executorService.execute(() -> {
+                try {
+                    semaphore.acquire();
+                    add(finalI);
+                    semaphore.release();
+                } catch (InterruptedException e) {
+                    log.error("exception", e);
+                }
+                countDownLatch.countDown();
+            });
+        }
+
+        countDownLatch.await();
+        executorService.shutdown();
+        log.info("{}", map.size());
+
+    }
+
+    private static void add(int i) {
+        map.put(i, i);
+    }
+}
---
+++ src/main/java/com/laokamo/concurrency/example/concurrent/ConcurrentSkipListSetExample.java
@@ -0,0 +1,49 @@
+package com.laokamo.concurrency.example.concurrent;
+
+import com.laokamo.concurrency.annoations.NotThreadSafe;
+import com.laokamo.concurrency.annoations.ThreadSafe;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.concurrent.*;
+
+@Slf4j
+@ThreadSafe
+public class ConcurrentSkipListSetExample {
+
+    // 请求总数
+    private static int clientTotal = 5000;
+    // 同时并发执行的线程数
+    private static int threadTotal = 200;
+
+    private static Set set = new ConcurrentSkipListSet();
+
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        final Semaphore semaphore = new Semaphore(threadTotal);
+        final CountDownLatch countDownLatch = new CountDownLatch(clientTotal);
+        for (int i = 0; i < clientTotal; i++) {
+            int finalI = i;
+            executorService.execute(() -> {
+                try {
+                    semaphore.acquire();
+                    add(finalI);
+                    semaphore.release();
+                } catch (InterruptedException e) {
+                    log.error("exception", e);
+                }
+                countDownLatch.countDown();
+            });
+        }
+
+        countDownLatch.await();
+        executorService.shutdown();
+        log.info("{}", set.size());
+
+    }
+
+    private static void add(int i) {
+        set.add(i);
+    }
+}
